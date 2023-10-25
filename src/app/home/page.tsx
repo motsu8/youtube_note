667a20d20cd8f1c96cc7646da60b718dd516b936
motsu8e <motsu159357@gmail.com>
2023-10-25 18:03:09 +0900

    ホーム画面の検索窓削除

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,23 +3,13 @@
 import { Session } from '@supabase/supabase-js';
 import React, { useEffect, useState } from 'react';
 
-import ConfirmVideo from '@/components/confirmVideo';
-// import DrawList from '@/components/drawList';
-import PopupContent from '@/components/parts/popupContent';
-import Search from '@/components/parts/search';
 import PostDocument from '@/components/postDocument';
-import { VideoData } from '@/types/components';
 import GetSession from '@/utils/sessionComponent';
 import SignOut from '@/utils/signOut';
 import supabase from '@/utils/supabaseClient';
 
-import Youtube from '../api/youtube';
-
 export default function Home() {
-  const [videoUrl, setVideoUrl] = useState('');
-  const [visible, setVisible] = useState(false);
   const [session, setSession] = useState<Session | null>(null);
-  const [videoData, setVideoData] = useState<VideoData | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -28,29 +18,11 @@ export default function Home() {
     })();
   }, []);
 
-  const submitAction = () => {
-    Youtube.getVideoSnippet(videoUrl, setVideoData);
-    setVisible(true);
-  };
-
   return (
     <div
       id="dashBoard"
       className="w-full relative flex flex-col justify-start items-center space-y-10 overflow-auto h-screen"
     >
-      <PopupContent visible={visible}>
-        <ConfirmVideo
-          videoData={videoData}
-          session={session}
-          setVideoData={setVideoData}
-          setVisible={setVisible}
-        />
-      </PopupContent>
-      <Search
-        placeholder="動画URLで追加"
-        setInputValue={setVideoUrl}
-        setSubmitAction={submitAction}
-      />
       <div className="space-x-3">
         <SignOut />
         <GetSession session={session} />
