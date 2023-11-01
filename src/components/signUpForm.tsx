b602c55608c54baa51142ec0193e8185ba876b4e
motsu8e <motsu159357@gmail.com>
2023-11-01 22:28:22 +0900

    バリデーション実装

--- src/components/signUpForm.tsx
+++ src/components/signUpForm.tsx
@@ -18,6 +18,14 @@ export default function SignUpForm() {
     isFilled ? 'bg-rose-300' : 'bg-neutral-50',
   ];
 
+  const emailRegex =
+    /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,16}$/;
+
+  const validation = (regex: RegExp, content: string) => {
+    return regex.test(content);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center shadow-lg space-y-3 py-5 px-3 w-1/5">
       <GoogleOauth />
@@ -42,18 +50,40 @@ export default function SignUpForm() {
           新規登録
         </button>
       </div>
-      <input
-        type="email"
-        className="bg-slate-100 px-3 py-1 w-5/6"
-        placeholder="メールアドレス"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        className="bg-slate-100 px-3 py-1 w-5/6"
-        placeholder="パスワード"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+
+      {/* email */}
+      <div className="w-5/6">
+        <input
+          type="email"
+          className="bg-slate-100 px-3 py-1 w-full"
+          placeholder="メールアドレス"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {validation(emailRegex, email) ? (
+          <p />
+        ) : (
+          <p className="text-red-100 text-xs">
+            有効なメールアドレスを入力してください
+          </p>
+        )}
+      </div>
+
+      {/* password */}
+      <div className="w-5/6">
+        <input
+          type="password"
+          className="bg-slate-100 px-3 py-1 w-full"
+          placeholder="パスワード"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {validation(passwordRegex, password) ? (
+          <p />
+        ) : (
+          <p className="text-red-100 text-xs">
+            大文字・小文字・数字を使用した8~16字
+          </p>
+        )}
+      </div>
 
       <IconButton
         icon={faArrowRight}
