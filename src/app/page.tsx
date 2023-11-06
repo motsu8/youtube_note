ecea2bdcd213d6bc6e080e4b3703fbf0af78e5e4
motsu8e <motsu159357@gmail.com>
2023-11-06 20:06:22 +0900

    LP画像のレスポンシブ対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,12 @@ export default function Landing() {
   return (
     <div className="w-full h-screen overflow-auto">
       <div id="lp-header" className="mx-2 p-10 h-1/6">
-        <h1 className="text-7xl">YouTube Note</h1>
-        <p className="text-3xl">
+        <h1 className="text-2xl lg:text-5xl xl:text-7xl">YouTube Note</h1>
+        <p className="text-lg lg:text-xl xl:text-3xl">
           YouTube動画を複合的なノートで学習することができます。
         </p>
       </div>
-      <div className="flex justify-around h-5/6 items-center">
+      <div className="flex flex-col lg:flex-row justify-around h-3/4 items-center px-10">
         <Slider />
         <SignUpForm />
       </div>
