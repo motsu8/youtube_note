56d50de8840b44d1c5abb6844d67bc545a78c195
motsu8e <motsu159357@gmail.com>
2023-12-16 17:49:51 +0900

    ノートセクションのホバーアニメーションの調整

--- src/components/parts/LP/noteCard.tsx
+++ src/components/parts/LP/noteCard.tsx
@@ -9,12 +9,13 @@ type NoteCard = {
 
 function NoteCard({ title, contents, url }: NoteCard) {
   return (
-    <div className="w-full flex flex-col justify-center items-center bg-main pb-10 space-y-5 rounded-tl-[32px] rounded-br-[32px] transition hover:scale-[1.02] duration-500">
+    <div className="w-full flex flex-col justify-center items-center bg-main pb-10 space-y-5 rounded-tl-[32px] rounded-br-[32px] overflow-hidden">
       <Image
         src={url}
         alt={title}
         height={500}
         width={1200}
+        className="hover:scale-[1.05] duration-500"
         style={{
           objectFit: 'cover',
           borderWidth: 2,
@@ -23,7 +24,7 @@ function NoteCard({ title, contents, url }: NoteCard) {
       />
       <div className="w-full flex flex-col justify-start px-5 space-y-3 pb-3">
         <p className="text-xl font-bold">{title}</p>
-        <p className='h-12'>{contents}</p>
+        <p className="h-12">{contents}</p>
       </div>
     </div>
   );
